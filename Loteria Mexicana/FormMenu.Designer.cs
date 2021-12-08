f8ae013529dc27775faba7718e1cf883bc1db600
AmadorLA <amadorlopezalejandro1@gmail.com>
2021-12-08 09:38:26 -0600

    Add project files.

---
+++ Loteria Mexicana/FormMenu.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Loteria_Mexicana
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnCartas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnIniciar.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnIniciar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnIniciar.Location = new System.Drawing.Point(616, 375);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(116, 50);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCartas
+            // 
+            this.btnCartas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCartas.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCartas.Location = new System.Drawing.Point(787, 375);
+            this.btnCartas.Name = "btnCartas";
+            this.btnCartas.Size = new System.Drawing.Size(99, 50);
+            this.btnCartas.TabIndex = 1;
+            this.btnCartas.Text = "Cartas";
+            this.btnCartas.UseVisualStyleBackColor = false;
+            this.btnCartas.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(959, 450);
+            this.Controls.Add(this.btnCartas);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "FormMenu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnCartas;
+    }
+}
+
---
+++ Loteria Mexicana/FormMenu.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Loteria_Mexicana
+{
+    public partial class FormMenu : Form
+    {
+        const int cantidad_cartas = 54;
+        private PictureBox[] tabla;
+
+        public FormMenu()
+        {
+            InitializeComponent(); ;
+            tabla = new PictureBox[25];
+            InicializarTabla();
+        }
+
+        private void InicializarTabla()
+        {
+            int r = 0, c = 0;
+            r = 0;
+            c = 0;
+
+            int[] cartas = new int[25];
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                cartas[i] = i + 1;
+            }
+
+            Random ra = new Random();
+
+            int a, aux;
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                a = ra.Next(cartas.Length);
+                aux = cartas[i];
+                cartas[i] = cartas[a];
+                cartas[a] = aux;
+            }
+
+            for (int i = 0; i < tabla.Length; i++)
+            {
+                tabla[i] = new PictureBox();
+                tabla[i].Location = new System.Drawing.Point(100 + (c * 90), 25 + (r * 125));
+                tabla[i].Name = "cartas" + i;
+                tabla[i].Size = new System.Drawing.Size(85, 120);
+                tabla[i].TabIndex = 0 + i;
+                tabla[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tabla[i].TabStop = false;
+                tabla[i].Image = Image.FromFile(@"cartas\)" + (cartas[i]) + ".jpg");
+                this.Controls.Add(tabla[i]);
+                c++;
+                if (c == 5)
+                {
+                    r++;
+                    c = 0;
+                }
+            }
+        }
+        private PictureBox[] baraja;
+        private void InicializarBaraja()
+        {
+             int[] cartas = new int[54];
+
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                cartas[i] = i + 1;
+            }
+            Random ra = new Random();
+            int a, aux;
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                a = ra.Next(cartas.Length);
+                aux = cartas[i];
+                cartas[i] = cartas[a];
+                cartas[a] = aux;
+            }
+
+            for (int i = 0; i < baraja.Length; i++)
+            {
+                baraja[i] = new PictureBox();
+                baraja[i].Location = new System.Drawing.Point (825, 45);
+                baraja[i].Name = "cartas" + i;
+                baraja[i].Size = new System.Drawing.Size(85, 120);
+                baraja[i].TabIndex = 0 + i;
+                baraja[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                baraja[i].TabStop = false;
+                baraja[i].Image = Image.FromFile(@"cartas\)" + (cartas[i]) + ".jpg");
+                this.Controls.Add(baraja[i]);
+                
+               
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InicializarTabla();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InicializarBaraja();               
+        }
+    }
+}
